1f9bdc840d210ad34d49873f9c361feb7195502e
Syrilai <30000694+Syrilai@users.noreply.github.com>
2025-08-22 20:09:59 +0200

    Change main service

--- MareSynchronos/WebAPI/SignalR/ApiController.cs
+++ MareSynchronos/WebAPI/SignalR/ApiController.cs
@@ -21,8 +21,8 @@ namespace MareSynchronos.WebAPI;
 #pragma warning disable MA0040
 public sealed partial class ApiController : DisposableMediatorSubscriberBase, IMareHubClient
 {
-    public const string MainServer = "Lunae Crescere Incipientis (Official Central Server)";
-    public const string MainServiceUri = "wss://maresynchronos.com";
+    public const string MainServer = "Gateway of Eternity";
+    public const string MainServiceUri = "wss://mare.syrilai.dev";
 
     private readonly DalamudUtilService _dalamudUtil;
     private readonly HubFactory _hubFactory;
