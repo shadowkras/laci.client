ae8382f1bf80355bb7d8f6c43782d3f6a8b70478
Shadow Kras <shadow_kras@hotmail.com>
2025-09-17 22:44:30 -0400

    feature: Added extension class for Running tasks as "fire and forget". This should be safer and log exceptions properly without propagating errors to other parts of the application.

---
+++ LaciSynchroni/Utils/TaskHelpers.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Runtime.CompilerServices;
+
+namespace LaciSynchroni.Utils
+{
+    /// <summary>
+    /// Helper methods for working with tasks.
+    /// </summary>
+    public static class TaskHelpers
+    {
+        /// <summary>
+        /// Run a task in the background, catching and logging any unhandled exceptions.
+        /// </summary>
+        public static void FireAndForget(Func<Task> taskFunc, ILogger? logger = null, CancellationToken? token = null, [CallerMemberName] string callerName = "")
+        {
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    if (token.HasValue)
+                        await taskFunc().WaitAsync(token.Value).ConfigureAwait(false);
+                    else
+                        await taskFunc().ConfigureAwait(false);
+                }
+                catch (OperationCanceledException)
+                {
+                    // Expected cancellation, ignore
+                }
+                catch (Exception ex)
+                {
+                    logger?.LogError(ex, "Unhandled exception in fire-and-forget task called by ({CallerName}): ", callerName);
+                }
+            });
+        }
+    }
+}
