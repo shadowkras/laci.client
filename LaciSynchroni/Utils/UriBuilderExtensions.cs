f8ad5cc0f51ff084200b20bf32a93435d7c4cbe6
Shadow Kras <shadow_kras@hotmail.com>
2025-09-17 22:46:23 -0400

    Feature: Added UriBuilderExtension class, with WsToHttp and HttpToWs, as proprosed by @Cytraen

---
+++ LaciSynchroni/Utils/UriBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace LaciSynchroni.Utils
+{
+    /// <summary>
+    /// Extensions for <see cref="UriBuilder"/>.
+    /// </summary>
+    public static class UriBuilderExtensions
+    {
+        public static UriBuilder WsToHttp(this UriBuilder uriBuilder)
+        {
+            if (string.Equals(uriBuilder.Scheme, Uri.UriSchemeWss, StringComparison.OrdinalIgnoreCase))
+                uriBuilder.Scheme = Uri.UriSchemeHttps;
+            else if (string.Equals(uriBuilder.Scheme, Uri.UriSchemeWs, StringComparison.OrdinalIgnoreCase))
+                uriBuilder.Scheme = Uri.UriSchemeHttp;
+
+            return uriBuilder;
+        }
+
+        public static UriBuilder HttpToWss(this UriBuilder uriBuilder)
+        {
+            if (string.Equals(uriBuilder.Scheme, Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase))
+                uriBuilder.Scheme = Uri.UriSchemeWss;
+            else if (string.Equals(uriBuilder.Scheme, Uri.UriSchemeHttp, StringComparison.OrdinalIgnoreCase))
+                uriBuilder.Scheme = Uri.UriSchemeWs;
+
+            return uriBuilder;
+        }
+    }
+}
